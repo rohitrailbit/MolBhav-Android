07cb6c77c87c03772fd0aef10742eb988c5645d6
abhigyanpandey5754 <137940778+abhigyanpandey5754@users.noreply.github.com>
2023-10-27 22:25:53 +0530

    Syncing Abhigyan Acer PC

---
+++ app/src/main/java/com/example/molbhav/adapters/ImageSliderAdapter.java
@@ -0,0 +1,53 @@
+package com.example.molbhav.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.molbhav.R;
+
+import java.util.List;
+
+public class ImageSliderAdapter extends PagerAdapter {
+
+    private final Context context;
+    private final List<Integer> imageList;
+
+    public ImageSliderAdapter(Context context, List<Integer> imageList) {
+        this.context = context;
+        this.imageList = imageList;
+    }
+
+    @Override
+    public int getCount() {
+        return imageList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.image_slider_item, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(imageList.get(position));
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
